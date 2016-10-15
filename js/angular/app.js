0bcaddedf9812b500b0fda5604c9edde8af2c4f2
manojbojja <manoj.bojja19@gmail.com>
2016-10-15 21:31:40 +0530

    all check

--- js/angular/app.js
+++ js/angular/app.js
@@ -1,11 +1,12 @@
 'use strict'
 
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['rzModule', 'ui.bootstrap']);
 
-myApp.controller('codeSubmissions',['$scope','$http', function($scope, $http){
+myApp.controller('ecommerceSubmissions',['$scope','$http', function($scope, $http){
 
     $http.get('https://hackerearth.0x10.info/api/fashion?type=json&query=list_products').success(function(data){
         $scope.submissions = data.products;
+        $scope.apiHits = data.quote_available;
     });
 
     $scope.counter = 0;  
@@ -13,150 +14,43 @@ myApp.controller('codeSubmissions',['$scope','$http', function($scope, $http){
     $scope.query = "";
     $scope.orderByModel = "";
 
+    $scope.minValue = 300;
+    $scope.maxValue = 4999;
+
+    $scope.minRangeSlider = {
+        minValue: 300,
+        maxValue: 4999,
+        options: {
+            floor: 300,
+            ceil: 7000,
+            step: 1
+        }
+    }
+
     $scope.customFilter = function(submission){
-        if ($scope.query.length > 0) {
-            if($scope.query.toLowerCase() == 'apparel' && submission.category == 0){
+        if($scope.minRangeSlider.minValue <= submission.price ){
+            if($scope.minRangeSlider.maxValue >= submission.price ){
+                if ($scope.query.length > 0) {
+                    if(('apparel').indexOf($scope.query.toLowerCase()) != -1 && submission.category == 0){
+                        return submission;
+                    }
+                    if(('accessories').indexOf($scope.query.toLowerCase()) != -1 && submission.category == 1){
+                        return submission;
+                    }
+                    if(submission.name.toLowerCase().indexOf($scope.query) == -1){
+                        return;
+                    }
+                }
                 return submission;
-            }
-            if($scope.query.toLowerCase() == 'accessories' && submission.category == 1){
-                return submission;
-            }
-            if(submission.name.toLowerCase().indexOf($scope.query) == -1){
-                return;
             }
         }
-        return submission;
+        return ;
     }
 
     $scope.parseId = function(val){
         val.price = parseInt(val.price);
+        val.rating = parseInt(val.rating);
         return val;
     }
-
-
-   
-
-    $scope.icons = [
-                        {
-                            "id": 1,
-                            "language": "GNU C++11",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/cpp.png"
-                        },
-                        {
-                            "id": 2,
-                            "language": "GNU C++",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/cpp.png"
-                        },
-                        {
-                            "id": 3,
-                            "language": "MS C++",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/cpp.png"
-                        },
-                        {
-                            "id": 4,
-                            "language": "Java 7",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/java.png"
-                        },
-                        {
-                            "id": 5,
-                            "language": "Python 3",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/python.svg"
-                        },
-                        {
-                            "id": 6,
-                            "language": "Java 8",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/java.png"
-                        },
-                        {
-                            "id": 7,
-                            "language": "MS C#",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/cs.png"
-                        },
-                        {
-                            "id": 8,
-                            "language": "FPC",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/pascal.png"
-                        },
-                        {
-                            "id": 9,
-                            "language": "Mono C#",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/cs.png"
-                        },
-                        {
-                            "id": 10,
-                            "language": "GNU C",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/c.png"
-                        },
-                        {
-                            "id": 11,
-                            "language": "Python 2",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/python.svg"
-                        },
-                        {
-                            "id": 12,
-                            "language": "GNU C11",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/c.png"
-                        },
-                        {
-                            "id": 13,
-                            "language": "Delphi",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/delphi.png"
-                        },
-                        {
-                            "id": 14,
-                            "language": "Ruby",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/ruby.png"
-                        },
-                        {
-                            "id": 15,
-                            "language": "PHP",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/php.png"
-                        },
-                        {
-                            "id": 16,
-                            "language": "PyPy 3",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/python.svg"
-                        },
-                        {
-                            "id": 17,
-                            "language": "JavaScript",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/javascript.png"
-                        },
-                        {
-                            "id": 18,
-                            "language": "D",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/d.png"
-                        },
-                        {
-                            "id": 19,
-                            "language": "PyPy 2",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/python.svg"
-                        },
-                        {
-                            "id": 20,
-                            "language": "Haskell",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/haskell.png"
-                        },
-                        {
-                            "id": 21,
-                            "language": "Scala",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/scala.png"
-                        },
-                        {
-                            "id": 22,
-                            "language": "Ocaml",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/ocaml.png"
-                        },
-                        {
-                            "id": 23,
-                            "language": "Perl",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/perl.png"
-                        },
-                        {
-                            "id": 24,
-                            "language": "Go",
-                            "icon": "https://hackerearth.0x10.info/dump/jda/language_icons/go_lang.png"
-                        }
-                    ];
 
 }]);
